245fe33107f2fc4ca658c0f852f6d9d339ef4c63
sunsonghao <songhaosun@gmail.com>
2022-10-06 18:10:10 +0800

    feat: 初始化项目配置

--- src/component/calculator.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Description: 状态提升
- * @Author: sunsh
- * @Date: 2022-09-29 18:29:13
- * @LastEditors: sunsh
- * @LastEditTime: 2022-09-29 18:33:16
- */
-import React from 'react';
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // React DOM 会负责更新 DOM 来与 React 元素保持一致
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM from 'react-dom/client';
+// import { ReactDOM } from 'react';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const ele = <div>
+  <h1>hello React</h1>
+  <p>我是react</p>
+</div>;
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(ele);
