df4d5a2274a4b715cda275cfccbd75681a9ced90
sunsonghao <songhaosun@gmail.com>
2022-10-17 15:08:29 +0800

    fix: 使用RTK静态添加分类

--- src/App.js
+++ src/App.js
@@ -3,24 +3,64 @@
  * @Author: sunsh
  * @Date: 2022-10-12 17:24:26
  * @LastEditors: sunsh
- * @LastEditTime: 2022-10-17 10:01:56
+ * @LastEditTime: 2022-10-17 14:53:10
  */
+import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { editName } from './store/articleSlice';
+import { addCategory } from './store/categorySlice'
+import { useGetCategoriesQuery } from "./store/cateoryApi";
 
 function App () {
-  const articleTitle = useSelector(state => state.article.title);
   const dispatch = useDispatch();
+  const articleTitle = useSelector(state => state.article.title);
+  const categoryList = useSelector(state => state.category);
+  const [categoryName, setCategoryName] = useState("");
+  const res = useGetCategoriesQuery();
+  console.log(res);
+  
 
   const handleClick = e => {
     dispatch(editName('我是新的标题'));
   }
 
+  const inputChangeHandle = useCallback(
+    (e) => {
+      setCategoryName(state => e.target.value);
+    },
+    [],
+  )
+
+  function addCategoryHandle(e) {
+    if (e.type === 'keydown' && e.keyCode !== 13) {
+      return;
+    }
+    dispatch(addCategory({name: categoryName}));
+    setCategoryName('');
+  }
+  
+
   return (
     <div onClick={ handleClick }>
       我是app and {articleTitle}
+      <div>
+        <label>
+          <span>分类: </span>
+          <input type={"text"} onKeyDown={addCategoryHandle} onChange={inputChangeHandle} value={ categoryName } />
+          <div>{ categoryName }</div>
+        </label>
+        <button onClick={addCategoryHandle}>增加分类</button>
+        
+        <ul>
+          {
+            categoryList.map((category, index) => (
+              <li key={index}>{category.name}</li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
-   );
+    );
 }
 
 export default App;
--- src/store/categorySlice.js
+++ src/store/categorySlice.js
@@ -3,12 +3,12 @@
  * @Author: sunsh
  * @Date: 2022-10-17 09:56:30
  * @LastEditors: sunsh
- * @LastEditTime: 2022-10-17 10:23:33
+ * @LastEditTime: 2022-10-17 11:13:28
  */
 
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = [{name: "未分类"}];
 
 const categorySlice = createSlice({
   name: "category",
@@ -16,9 +16,16 @@ const categorySlice = createSlice({
   reducers: {
     setName(state, action) {
       state.name = action.payload; // immer库代理对象，只在createSlice（RTK）中可直接赋值
+    },
+    addCategory(state, action) {
+      let payload = action.payload || {name: ''};
+      if (payload != null && !!payload.name) {
+        state.push(action.payload);
+      }
     }
+    
   }
 });
 
-export const { setName: setCategoryName } = categorySlice.actions;
+export const { setName: setCategoryName, addCategory } = categorySlice.actions;
 export const { reducer: categoryReducer } = categorySlice;
